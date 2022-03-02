e24c2593f476fe81d67c35650239eacfb3abb458
agrawalshanu708 <95335996+agrawalshanu708@users.noreply.github.com>
2022-03-02 16:35:56 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,47 @@
+import { useEffect, useState } from "react";
+import "./styles.css";
+
+export default function App() {
+  const [change, setChange] = useState("");
+  const [wish, setWish] = useState([]);
+
+  useEffect(() => {
+    console.log(`getting`);
+    setWish(JSON.parse(localStorage.getItem("wish")));
+  }, []);
+
+  useEffect(() => {
+    console.log(`setting`);
+    localStorage.setItem("wish", JSON.stringify(wish));
+  }, [wish]);
+
+  return (
+    <div className="App">
+      <h1>WishBox!!</h1>
+      <form>
+        <input
+          value={change}
+          type="text"
+          onChange={(e) => setChange(e.target.value)}
+        />
+        <button
+          type="submit"
+          onClick={(e) => {
+            e.preventDefault();
+            setWish((prevWish) => {
+              setChange("");
+              return [...prevWish, change];
+            });
+          }}
+        >
+          Add
+        </button>
+      </form>
+      <div className="display">
+        {wish.map((item) => (
+          <li>{item}</li>
+        ))}
+      </div>
+    </div>
+  );
+}
